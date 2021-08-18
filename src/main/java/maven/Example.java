eb4e9bc1f7f3ecd2bf7acce7b944dee2ae064b5f
courtnieferguson <84743680+courtnieferguson@users.noreply.github.com>
2021-08-18 10:30:29 -0500

    first exercise

---
+++ src/main/java/maven/Example.java
@@ -0,0 +1,21 @@
+package maven;
+
+import org.apache.commons.lang3.StringUtils
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Example {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Give us a word: ");
+        String usersWord = input.nextLine();
+        System.out.printf("You Entered: %s\n" ,usersWord);
+        System.out
+    }
+
+
+}
